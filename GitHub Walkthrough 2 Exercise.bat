865578cfd504d68f3ea64660e977a2281ee7c142
leo.ocallaghan <L00177540@atu.ie>
2022-12-26 16:24:10 +0000

    Add GitHub Walkthrough 2 Exercise.bat

---
+++ GitHub Walkthrough 2 Exercise.bat
@@ -0,0 +1,21 @@
+# By: LOC
+# Date: 27/11/2022
+# Function: Perform a commit
+# Script: Walkthough 2 Exercise.bat
+git status
+echo "**************************************************"
+echo Performing an add for all files in this directory"
+echo "**************************************************"
+git add .
+git status
+echo "**************************************************"
+set /p MESSAGE=Please enter the commit message:
+git commit -m "$MESSAGE"
+git status
+echo "**************************************************"
+echo "Pushing to GITHUB repository"
+git push -u origin main
+echo "**************************************************"
+echo "Done!"
+cmd /k
+
