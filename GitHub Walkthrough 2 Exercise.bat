6f99e8e55200b61b2ee3519d54e0ae16a2535760
Leo O'Callaghan <118669443+l00177540@users.noreply.github.com>
2022-12-26 16:28:35 +0000

    Update GitHub Walkthrough 2 Exercise.bat

--- GitHub Walkthrough 2 Exercise.bat
+++ GitHub Walkthrough 2 Exercise.bat
@@ -2,6 +2,8 @@
 # Date: 27/11/2022
 # Function: Perform a commit
 # Script: Walkthough 2 Exercise.bat
+# This is the .bat file version of the Linux shell
+# script provided on page 12 of Git Walkthrough 3
 git status
 echo "**************************************************"
 echo Performing an add for all files in this directory"
