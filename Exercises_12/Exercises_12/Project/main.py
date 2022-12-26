542adf2468e8c1aa3788902602691ee165004d97
Leo O'Callaghan <118669443+l00177540@users.noreply.github.com>
2022-12-26 08:56:58 +0000

    Delete Exercises_12/Exercises_12 directory

--- Exercises_12/Exercises_12/Project/main.py
+++
@@ -1,9 +0,0 @@
-"""
-main.py
-By: JOR
-Date: 20OCT22
-"""
-from Source.directory_utilities import detect_os, detect_working_directory
-# Typo in notes corrected "Source.diretory_utilities"
-print(detect_os())
-print(detect_working_directory()) 
